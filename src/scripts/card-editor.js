2d122895f48341ea4d1454ac077d1be0554ac680
root <root@DESKTOP-GK8T5KQ.localdomain>
2020-02-23 21:16:43 +0100

    Recreated pure canvas to fabric.js

--- src/scripts/card-editor.js
+++ src/scripts/card-editor.js
@@ -1,172 +1,158 @@
 import { fabric } from "fabric";
 
-console.log('r/place');
+const WIDTH = 650;
+const HEIGHT = 1016;
 
-
-// create a wrapper around native canvas element (with id="c")
-const width = 650;
-const height = 1016;
 var canvas = new fabric.Canvas('c');
-canvas.setWidth(width);
-canvas.setHeight(height);
 
+canvas.setWidth(WIDTH);
+canvas.setHeight(HEIGHT);
 
-// create a rectangle object
-var rect = new fabric.Rect({
-    left: 0,
-    top: 0,
-    width: width,
-    height: height,
-    stroke: 'red',
-    strokeWidth: 15,
-    rx:15,
-    ry:1
-});
+drawBossTemplate(); // TODO remove not common part - images, text from template and use it later
 
-rect.set('selectable', true); 
+/*
+A plan is following:
+    1. User select from the list of existing templates
+    2. depending on the template user should be able to provide text, effects, images and properties on editor
+    3. user may choose card specific properties and mechanics - including special effects
+    4. user may save a card as a draft or public to existing decks - (see label on the bottom)
+*/
 
-// "add" rectangle onto canvas
-canvas.add(rect);
-canvas.renderAll();
-
-
-// var canvas = document.querySelector('canvas');
-// var canvas = document.getElementById('fg_layer')
-// var canvas_image = document.getElementById('bg_layer')
-
-
-// canvas.width = 650;
-// canvas.height = 1016;
-// canvas_image.width = 650;
-// canvas_image.height = 1016;
-
-
-// var ctx = canvas.getContext('2d');
-// var bf_ctx = canvas.getContext('2d');
-
-// ctx.fillStyle = "#e6daa6";
-// ctx.fillRect(0, 0, 650, 1016);
-// drawImage(55, 100, 250, 250, 'source', bf_ctx);
-// drawBorder(0, 0, canvas.width, canvas.height, 25, "#25120c");
-// drawBorder(25, 25, canvas.width - 25, canvas.height - 25, 25, "#c94a3b");
-// drawLabel(canvas.width / 4 + 2, 30, 300, 60, "#cb483b");
-// drawText(canvas.width / 4 + 95, 75, "BOSS", "45px Georgia", "rgb(0, 0, 0)");
-
-// drawLabel(canvas.width / 2 - 20, canvas.height - 70, 50, 50, "#25120c");
-// drawText(canvas.width / 2 - 17, canvas.height - 30, "M", "45px Georgia", "rgb(0, 0, 0)");
-// // drawHelpLines(15);
-// drawBossName();
-// drawText(330, 200, "+2", "22px Georgia", "rgb(0, 0, 0)");
-// drawImage(360, 165, 45, 45, 'fists_img', ctx);
-// drawText(405, 200, "za każdy Przedmiot", "22px Georgia", "rgb(0, 0, 0)");
-// drawText(330, 225, "na +0, walczących graczy", "22px Georgia", "rgb(0, 0, 0)");
-
-// function drawBossName() {
-//     drawText(350, 150, "BHEG", "60px Georgia", "rgb(0, 0, 0)");
-// }
-
-
-
-// function drawImage(x, y, width, height, imgId, context) {
-//     // TODO opacity and transparency of the image
-//     // ctx.globalAlpha = 0.5;
-//     const image = document.getElementById(imgId);
-//     context.drawImage(image, x, y, width, height);
-//     // ctx.globalAlpha = 1;
-// }
-
-// function drawLabel(x, y, width, height, fillStyle) {
-//     ctx.fillStyle = fillStyle;
-//     var radius = 15;
-//     ctx.beginPath();
-//     ctx.moveTo(x + width - radius, y);
-//     ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-//     ctx.lineTo(x + width, y + height - radius);
-//     ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-//     ctx.lineTo(x + radius, y + height);
-//     ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-//     ctx.lineTo(x, y + radius);
-//     ctx.quadraticCurveTo(x, y, x + radius, y);
-//     ctx.fill();
-//     ctx.closePath();
-
-// }
-
-// function drawText(x, y, text, font, fillStyle) {
-//     ctx.font = font;
-//     ctx.fillStyle = fillStyle;
-//     ctx.fillText(text, x, y);
-// }
-
-// function drawHelpLines(radius) {
-//     ctx.beginPath();
-//     var hy = 0;
-//     while (hy < canvas.height) {
-//         ctx.moveTo(0, hy);
-//         ctx.lineTo(canvas.width, hy);
-//         hy += radius;
-//     }
-
-//     var hy = 0;
-//     while (hy < canvas.height) {
-//         ctx.moveTo(0 + hy, 0);
-//         ctx.lineTo(0 + hy, canvas.height);
-//         hy += radius;
-//     }
-//     ctx.stroke();
-//     ctx.closePath();
-// }
-
-// function drawBorder(x, y, borderWidth, borderHeight, radius, fillStyle) {
-//     ctx.beginPath();
-//     ctx.fillStyle = fillStyle;
-//     ctx.moveTo(x + radius, y);
-//     ctx.lineTo(borderWidth - radius, y);
-//     ctx.quadraticCurveTo(borderWidth, y, borderWidth, y + radius);
-//     ctx.lineTo(borderWidth, (y + borderHeight) / 2);
-//     ctx.lineTo(borderWidth - radius, (y + borderHeight) / 2);
-//     ctx.lineTo(borderWidth - radius, y + radius * 2);
-//     ctx.quadraticCurveTo(borderWidth - radius, y + radius, borderWidth - radius * 2, y + radius);
-//     ctx.lineTo(x + radius * 2, y + radius);
-//     ctx.quadraticCurveTo(x + radius, y + radius, x + radius, y + radius * 2);
-//     ctx.lineTo(x + radius, (y + borderHeight) / 2);
-//     ctx.lineTo(x, (y + borderHeight) / 2);
-//     ctx.lineTo(x, y + radius);
-//     ctx.quadraticCurveTo(x, y, x + radius, y);
-
-//     ctx.moveTo(x + radius, borderHeight);
-//     ctx.lineTo(borderWidth - radius, borderHeight);
-//     ctx.quadraticCurveTo(borderWidth, borderHeight, borderWidth, borderHeight - radius);
-//     ctx.lineTo(borderWidth, (y + borderHeight) / 2);
-//     ctx.lineTo(borderWidth - radius, (y + borderHeight) / 2);
-//     ctx.lineTo(borderWidth - radius, borderHeight - radius * 2);
-//     ctx.quadraticCurveTo(borderWidth - radius, borderHeight - radius, borderWidth - radius * 2, borderHeight - radius);
-//     ctx.lineTo(x + radius * 2, borderHeight - radius);
-//     ctx.quadraticCurveTo(x + radius, borderHeight - radius, x + radius, borderHeight - radius * 2);
-//     ctx.lineTo(x + radius, (y + borderHeight) / 2);
-//     ctx.lineTo(x, (y + borderHeight) / 2);
-//     ctx.lineTo(x, borderHeight - radius);
-//     ctx.quadraticCurveTo(x, borderHeight, x + radius, borderHeight);
-//     ctx.fill();
-//     // ctx.stroke();
-//     ctx.closePath();
-
-// }
-
-// console.log(canvas);
-
-
-// // change color on button click
-// document.getElementById('button_previous')
-//     .addEventListener('click', function (event) {
-//         ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-//         ctx.fillRect(0, 0, 650, 1016);
-//     });
-// // change color on button click
-// document.getElementById('button_next')
-//     .addEventListener('click', function (event) {
-//         ctx.fillStyle = 'rgba(0, 255, 0, 1)';
-//         ctx.fillRect(0, 0, 650, 1016);
-//     });
-window.onload = function () {
+function drawBossTemplate() {
+    drawBackground('#e6daa6');
+    drawBorder();
+    drawLabel("BOSS", 20, 50, 60, '#c94a3b');
+    drawCirclePortrait('../public/assets/images/1.png');
+    drawLabel("DRAFT", 965, 10, 30, '#c94a3b');
+    drawBossName('BHEG');
 }
+
+
+function drawBossName(name) {
+    var bossName = new fabric.Text(name,
+        {
+            left: 380,
+            top: 100,
+            fontSize: 60,
+            fill: '#25120c',
+        });
+    canvas.add(bossName);
+}
+
+function drawCirclePortrait(url) {
+    var width = 250;
+    fabric.Image.fromURL(url,
+        function (oImg) {
+            oImg.clipPath = new fabric.Circle(
+                {
+                    radius: width / 2,
+                    originX: 'center',
+                    originY: 'center'
+                }
+            );
+
+            oImg.scale(1.0).set({
+                width: width,
+                height: width,
+                originX: 'center',
+                originY: 'center'
+            });
+
+            var stroke = new fabric.Circle(
+                {
+                    radius: (width / 2) + 5,
+                    originX: 'center',
+                    originY: 'center',
+                    fill: '#25120c'
+                }
+            );
+            canvas.add(stroke);
+            canvas.add(oImg);
+
+            var group = new fabric.Group([stroke, oImg], {
+                left: 45,
+                top: 90,
+                selectable: false
+            });
+            canvas.add(group);
+        });
+}
+
+function drawLabel(text, height, margin, fontSize, backgroundColor) {
+    var text = new fabric.Text(text,
+        {
+            fontSize: fontSize,
+            fill: '#25120c',
+            originX: 'center',
+            originY: 'center'
+        });
+
+    var rect = new fabric.Rect({
+        width: text.width + margin * 2,
+        height: text.height,
+        fill: backgroundColor,
+        originX: 'center',
+        originY: 'center',
+        rx: 15,
+        ry: 0
+    });
+
+    var group = new fabric.Group([rect, text], {
+        left: 0,
+        top: height,
+    });
+
+    canvas.add(group);
+    group.centerH();
+    return group;
+}
+
+function drawBackground(color) {
+    var margin = 15;
+    var marginLeft = margin;
+    var marginTop = margin;
+    var width = WIDTH - marginLeft - margin;
+    var height = HEIGHT - marginTop - margin;
+
+    canvas.add(
+        new fabric.Rect({
+            left: marginLeft,
+            top: marginTop,
+            width: width,
+            height: height,
+            fill: color,
+
+        }));
+}
+
+function drawBorder() {
+    var outerBorder = (drawBorder(0, 0, WIDTH, HEIGHT, 30));
+    outerBorder.stroke = '#25120c';
+
+    var innerBorder = drawBorder(15, 15, WIDTH - 30, HEIGHT - 30, 15);
+    innerBorder.stroke = '#c94a3b';
+
+    canvas.add(outerBorder);
+    canvas.add(innerBorder);
+
+    function drawBorder(x, y, width, height, stroke) {
+        return new fabric.Rect({
+            left: x,
+            top: y,
+            width: width - stroke,
+            height: height - stroke,
+            stroke: 'black',
+            strokeWidth: stroke,
+            fill: 'rgba(0,0,0,0)',
+
+            rx: 15,
+            ry: 0
+        });
+    }
+}
+
+canvas.selection = false;
+canvas.forEachObject(function (o) {
+    o.selectable = false;
+});
+canvas.renderAll();
