48d829ed0933752916e8ecbfbdaa9693c549e14e
Daniel Macikowski <daniel.macikowski@gmail.com>
2020-02-26 23:00:14 +0100

    Added Boss stats - hardcoded yet

--- src/scripts/card-editor.js
+++ src/scripts/card-editor.js
@@ -11,11 +11,11 @@ canvas.setHeight(HEIGHT);
 drawBossTemplate(); // TODO remove not common part - images, text from template and use it later
 
 /*
-A plan is following:
-    1. User select from the list of existing templates
-    2. depending on the template user should be able to provide text, effects, images and properties on editor
-    3. user may choose card specific properties and mechanics - including special effects
-    4. user may save a card as a draft or public to existing decks - (see label on the bottom)
+    A plan is following:
+        1. User select from the list of existing templates
+        2. depending on the template user should be able to provide text, effects, images and properties on editor
+        3. user may choose card specific properties and mechanics - including special effects
+        4. user may save a card as a draft or public to existing decks - (see label on the bottom)
 */
 
 function drawBossTemplate() {
@@ -25,6 +25,119 @@ function drawBossTemplate() {
     drawCirclePortrait('../public/assets/images/1.png');
     drawLabel("DRAFT", 965, 10, 30, '#c94a3b');
     drawBossName('BHEG');
+    drawDamageBox()
+}
+
+function drawDamageBox() {
+
+    let margin = 40;
+    let boxTop = 400;
+    let boxHeight = 240;
+
+    var rect = new fabric.Rect({
+        width: WIDTH - margin * 2,
+        height: boxHeight,
+        stroke: '#25120c',
+        strokeWidth: 2,
+        fill: 'white',
+        rx: 15,
+        ry: 0
+    });
+    canvas.add(rect);
+
+    var group = new fabric.Group([rect], {
+        left: margin,
+        top: boxTop,
+        selectable: false
+    });
+
+    var centerPoint = group.getCenterPoint();
+
+    fabric.Image.fromURL('../public/assets/images/players.png',
+        (imgPlayers) => {
+            const width = 512;
+            imgPlayers.scale(0.1).set({
+                left: 70,
+                top: centerPoint.y - 70,
+                width: width,
+                height: width,
+                originX: 'center',
+                originY: 'center'
+            });
+            canvas.add(imgPlayers);
+            group.addWithUpdate(imgPlayers);
+        });
+
+    fabric.Image.fromURL('../public/assets/images/sword.png',
+        (imgDamage) => {
+            const width = 256;
+            imgDamage.scale(0.18).set({
+                left: 70,
+                top: centerPoint.y,
+                width: width,
+                height: width,
+                originX: 'center',
+                originY: 'center'
+            });
+            canvas.add(imgDamage);
+            group.addWithUpdate(imgDamage);
+        });
+
+    fabric.Image.fromURL('../public/assets/images/hp.png',
+        (imgDamage) => {
+            const width = 512;
+            imgDamage.scale(0.08).set({
+                left: 70,
+                top: centerPoint.y + 70,
+                width: width,
+                height: width,
+                originX: 'center',
+                originY: 'center'
+            });
+            canvas.add(imgDamage);
+            group.addWithUpdate(imgDamage);
+        });
+
+
+    const playerNumbers = ['3', '4', '5-6', '7-8'];
+    const dps = ['35', '50', '75', '100'];
+    const hp = ['45', '60', '90', '120'];
+    const pixelsAboveBox = 70;
+    for (var i = 0; i < playerNumbers.length; i++) {
+        var playerNumber = new fabric.Text(playerNumbers[i],
+            {
+                left: 150 + 130 * i,
+                top: centerPoint.y - pixelsAboveBox,
+                fill: '#25120c',
+                originX: 'center',
+                originY: 'center'
+            });
+        canvas.add(playerNumber);
+        group.addWithUpdate(playerNumber);
+        var dpsNumber = new fabric.Text(dps[i],
+            {
+                left: 150 + 130 * i,
+                top: centerPoint.y,
+                fill: '#25120c',
+                originX: 'center',
+                originY: 'center'
+            });
+        canvas.add(dpsNumber);
+        group.addWithUpdate(dpsNumber);
+        var hpNumber = new fabric.Text(hp[i],
+            {
+                left: 150 + 130 * i,
+                top: centerPoint.y + 70,
+                fill: '#25120c',
+                originX: 'center',
+                originY: 'center'
+            });
+        canvas.add(hpNumber);
+        group.addWithUpdate(hpNumber);
+    }
+
+
+    canvas.add(group);
 }
 
 
@@ -144,7 +257,6 @@ function drawBorder() {
             stroke: 'black',
             strokeWidth: stroke,
             fill: 'rgba(0,0,0,0)',
-
             rx: 15,
             ry: 0
         });
